cbe6f10876a8161b273ecd55753f8316a9eb168f
OxygenMemo <g58160698@gmail.com>
2017-11-13 22:33:35 +0700

    g

---
+++ Lab09_sqlite/app/src/main/java/com/example/apple/lab09_sqlite/DatabaseHelper.java
@@ -0,0 +1,32 @@
+package com.example.apple.lab09_sqlite;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Apple on 11/8/2017 AD.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+    private static final String DB_NAME = "memberDB";
+    private static final int DB_VERSION = 1 ;
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
